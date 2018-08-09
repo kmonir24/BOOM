7f9df589ffbb269bf377c8d721bf5a7576fb1c98
zhangdzh <41393175+zhangdzh@users.noreply.github.com>
2018-08-09 15:46:30 -0400

    Update eventsJAVA.js

--- eventsJAVA.js
+++ eventsJAVA.js
@@ -16,8 +16,55 @@ function openPage(pageName, elmnt, color) {
     document.getElementById(pageName).style.display = "block";
 
     // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
+    element.style.backgroundColor = color;
 }
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+
+// button stuff
+filterSelection("browsemore")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "browsemore") c = "";
+
+  for (i = 0; i < x.length; i++) {
+    RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show")
+  }
+}
+
+function AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1=element.className.split(" ");
+  arr2=name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1=element.className.split(" ");
+  arr2=name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// function showContent() {
+//     document.getElementById"show1").style.visibility = "visible";
+//     document.getElementById"show2").style.visibility = "visible";
+// }
+
+//
+// // stuff for buttons idk
+// var buttoncontainer = document.getElementById("mybuttoncontainer");
+// var btns = buttoncontainer.getElementsByClassName("buttons")

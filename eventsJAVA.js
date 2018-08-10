4ec39b1b24b5f1deef159085d6ee818a4d8f922d
zhangdzh <41393175+zhangdzh@users.noreply.github.com>
2018-08-10 15:02:04 -0400

    Update eventsJAVA.js

--- eventsJAVA.js
+++ eventsJAVA.js
@@ -16,7 +16,7 @@ function openPage(pageName, elmnt, color) {
     document.getElementById(pageName).style.display = "block";
 
     // Add the specific color to the button used to open the tab content
-    element.style.backgroundColor = color;
+    elmnt.style.backgroundColor = color;
 }
 
 // Get the element with id="defaultOpen" and click on it
@@ -24,40 +24,40 @@ document.getElementById("defaultOpen").click();
 
 
 // button stuff
-filterSelection("browsemore")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "browsemore") c = "";
-
-  for (i = 0; i < x.length; i++) {
-    RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show")
-  }
-}
-
-function AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1=element.className.split(" ");
-  arr2=name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
-
-function RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1=element.className.split(" ");
-  arr2=name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
+// filterSelection("browsemore")
+// function filterSelection(c) {
+//   var x, i;
+//   x = document.getElementsByClassName("filterDiv");
+//   if (c == "browsemore") c = "";
+//
+//   for (i = 0; i < x.length; i++) {
+//     RemoveClass(x[i], "show");
+//     if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show")
+//   }
+// }
+//
+// function AddClass(element, name) {
+//   var i, arr1, arr2;
+//   arr1=element.className.split(" ");
+//   arr2=name.split(" ");
+//   for (i = 0; i < arr2.length; i++) {
+//     if (arr1.indexOf(arr2[i]) == -1) {
+//       element.className += " " + arr2[i];
+//     }
+//   }
+// }
+//
+// function RemoveClass(element, name) {
+//   var i, arr1, arr2;
+//   arr1=element.className.split(" ");
+//   arr2=name.split(" ");
+//   for (i = 0; i < arr2.length; i++) {
+//     while (arr1.indexOf(arr2[i]) > -1) {
+//       arr1.splice(arr1.indexOf(arr2[i]), 1);
+//     }
+//   }
+//   element.className = arr1.join(" ");
+// }
 
 // function showContent() {
 //     document.getElementById"show1").style.visibility = "visible";
@@ -68,3 +68,53 @@ function RemoveClass(element, name) {
 // // stuff for buttons idk
 // var buttoncontainer = document.getElementById("mybuttoncontainer");
 // var btns = buttoncontainer.getElementsByClassName("buttons")
+
+
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}

08fdf5ae721ca4120e399758b86611dbfe65061e
zhangdzh <41393175+zhangdzh@users.noreply.github.com>
2018-08-13 21:53:26 -0400

    Update eventsJAVA.js

--- eventsJAVA.js
+++ eventsJAVA.js
@@ -71,3 +71,19 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+// collapsible java
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active1");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
